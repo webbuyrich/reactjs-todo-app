492d2101e33b87713934bc051644395a0b7e0caf
webbuyrich <webbuyrich@gmail.com>
2019-09-11 03:21:36 -0700

    added constructor with todo state, JSON.parse todos, update state using this.setState

--- src/App.js
+++ src/App.js
@@ -6,20 +6,33 @@ import './App.css';
 
 class App extends React.Component{
 
+  constructor(){
+    super();
+    this.state ={
+      todos: []
+    };
+  }
+
   render(){
     return(
       <div>
-          <TodoList></TodoList>
-          <TodoItem></TodoItem>
-      </div>)
+          {
+            2 +2 
+          }
+      </div>
+      );
   }
 
   // when component is rendered and put into the DOM
   componentDidMount = () => {
     // see if there is anything in local storage about todos
+    // must change object into string before putting into local storage - use JSON.stringify
+    // must change string into object when it comes out - use JSON.parse
     const todos = localStorage.getItem('todos');
     if(todos){
-      console.log('Had todos', todos);
+      const savedTodos = JSON.parse(todos);
+      // update state - state is an asynchronus function
+      this.setState({ todos: savedTodos });
     } else{
       console.log('No todos');
     }
