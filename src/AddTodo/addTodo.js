2e760d41ec3448237b5b64d46c955a884aab1379
webbuyrich <webbuyrich@gmail.com>
2019-09-11 03:46:11 -0700

    added constructor with todo state, setState of todo to e.target.value, logged the state after submit

--- src/AddTodo/addTodo.js
+++ src/AddTodo/addTodo.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+// This will update the state when adding a Todo object
 class AddTodo extends React.Component{
+
+	constructor(){
+		super();
+		this.state ={
+			todo: ''
+		};
+	}
 
  	render(){
  		return(
  			<div className="">
  				<form onSubmit={(e) => this.submitTodo(e)} >
- 					<input 	onChange={(e) => this.updateInput(e)} type='text'></input>
+ 					<input onChange={(e) => this.updateInput(e)} type='text'></input>
  					<button type="submit">Add Todo</button>
  				</form>
  			</div>
@@ -15,14 +23,16 @@ class AddTodo extends React.Component{
 
  	// function to update input onclick
  	updateInput = (e) =>{
- 		// log input each time a character is entered
- 		console.log(e);
+ 		/*log input each time a character is entered
+ 		console.log(e);*/
+ 		this.setState({ todo: e.target.value });
  	}
 
  	// function to submit the form
  	submitTodo = (e) => {
+ 		// prevents form from refreshing the page on submission
  		e.preventDefault();
- 		console.log('submit');
+ 		console.log('submit', this.state);
  	}
 
 }
